38864d104e37f075a4f7fdfb78d126f90c4f9823
cosmopoliitans <elfkyn52@gmail.com>
2023-05-01 16:45:37 +0300

    esnek yapıldı

--- src/App.js
+++ src/App.js
@@ -10,17 +10,37 @@ import ShoppingCart from "./components/ShoppingCart";
 
 function App() {
   const [products, setProducts] = useState(data);
-  const [cart, setCart] = useState([]);
+  const [cart, setCart] = useState(initialStateOku("cart"));
 
+  function localStorageYaz(cart) {
+    return localStorage.setItem("cart", JSON.stringify(cart));
+  }
+  function localStorageOku(key) {
+    return JSON.parse(localStorage.getItem(key));
+  }
+  function initialStateOku(key) {
+    const initialCart = localStorageOku(key);
+    if (initialCart) {
+      return initialCart;
+    } else {
+      return [];
+    }
+  }
   const addItem = (item) => {
     // verilen itemi sepete ekleyin
     const newCart = [...cart, item];
     setCart(newCart);
+    localStorageYaz(newCart);
   };
 
+  const deleteItem = (item) => {
+    const deleteItem = [...cart.filter((i) => i.id !== item.id)];
+    setCart(deleteItem);
+    localStorageYaz(deleteItem);
+  };
   return (
     <ProductContext.Provider value={{ products, addItem }}>
-      <CartContext.Provider value={{ cart }}>
+      <CartContext.Provider value={{ cart, deleteItem }}>
         <div className="App">
           <Navigation />
 
